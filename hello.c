29a78023453286c40e2d672a9de9fd625ff3a31c
David Ryckman <dryckman2@huskers.unl.edu>
2020-08-18 01:53:19 +0000

    intital commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: David Ryckman
+ * Date:    8/17/2020
+ *
+ * simple hello world in c
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main (int argc, char **argv){
+
+     printf("Hello World!\n");
+
+     return 0;
+
+ }
